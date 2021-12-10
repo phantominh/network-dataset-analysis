5676949558057ea2a1fbad72b62cab080828df46
dvalen25 <dvalen25@illinois.edu>
2021-12-09 23:51:58 -0600

    fixed conflict

--- tests/graph_test.cpp
+++ tests/graph_test.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
@@ -78,43 +77,4 @@ TEST_CASE("insertEdge works correctly") {
     // TODO: minhtp2
 }
 
-=======
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-#include <vector>
-#include "../graph.h"
-
-// TODO: Delete this after you complete at least 1 test case, else Makefile linker will break
-TEST_CASE("Foo") {
-    REQUIRE(1==1);
-}
-
-// TODO: Some data here for testing (vector of edge pairs)
-// NOTE: This is a directed graph, so please check and make sure rules we apply are correct!!!
-
-TEST_CASE("Constructor parses the data correctly from a given vector of edge pairs") {
-    // TODO: dvalen25
-}
-
-TEST_CASE("insertVertex works correctly") {
-    // TODO: dvalen25
-}
-
-TEST_CASE("adjacentVertexes works correctly") {
-    // TODO: dvalen25
-}
-
-TEST_CASE("areAdjacent on 2 adjacent vertexes") {
-    // TODO: dvalen25
-}
-
-TEST_CASE("areAdjacent on 2 NOT adjacent vertexes") {
-    // TODO: dvalen25
-}
-
-TEST_CASE("insertEdge works correctly") {
-    // TODO: davlen25
-}
-
->>>>>>> main
 // TODO: Additional edge cases that you can think of ...
